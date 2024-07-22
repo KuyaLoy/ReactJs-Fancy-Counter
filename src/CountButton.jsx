8a185871e53b48c602ac96d9d7c4bb17fcc57aeb
Robin Tapiru <robintapiru0894@gmail.com>
2024-07-23 00:23:51 +0400

    Finish Refactoring Reusable Button Component

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,21 +1,25 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export default function CountButton({ type, setCount }) {
+  const handleClick = () => {
+    setCount((prev) => {
+      if (type === "minus") {
+        return prev - 1;
+      } else {
+        return prev + 1;
+      }
+    });
+  };
   return (
-    <button
-      className="count-btn"
-      onClick={() => {
-        setCount((prev) => prev + 1);
-      }}
-    >
-      {/* {type === "minus" ? (
+    <button className="count-btn" onClick={handleClick}>
+      {type === "minus" ? (
         <MinusIcon className="count-btn-icon" />
       ) : (
         <PlusIcon className="count-btn-icon" />
-      )} */}
+      )}
 
-      {type == "minus" && <MinusIcon className="count-btn-icon" />}
-      {type == "plus" && <PlusIcon className="count-btn-icon" />}
+      {/* {type == "minus" && <MinusIcon className="count-btn-icon" />} */}
+      {/* {type == "plus" && <PlusIcon className="count-btn-icon" />} */}
     </button>
   );
 }
